da6833c699bb1567dce6ad6e74dac36d2ed13cf1
scientistsagoemarcus <Kwakuabeikusagoe@gmail.com>
2021-12-02 14:57:46 +0000

    commit to master

--- index.js
+++ index.js
@@ -9,20 +9,39 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 //configuring the env
 dotenv.config();
+//importing the todo from our model package
+import { Todo } from "./models/todo.js";
 
 //creating an instance of the express package
 const app =express();
 
+//decoding the body using the express.json middleware
+app.use(express.json());
+
 //choosing a port to start our server
 const PORT = process.env.PORT?? 5000;
-
-
 
 //listen to an incoming resqust from the server
 app.get("/",(request,response)=>{
     response.send("Backend Developers")
 
 });
+//posting to our database using asyncronous function
+app.post('/todo',async(req, res)=>{
+   const {title,description, dateTime,isccomplete}=req.body;
+
+   const newtodo=Todo({
+    title:title,
+    description:description,
+     dateTime:dateTime,
+     isComplete:isComplete
+});
+//saves the todo model and awaits the results
+const result= await newtodo.save();
+req.json(result); 
+
+});
+
 //
 mongoose.connect(process.env.MONGO_DB_CONSTRING, (error)=>{
 if(error){
